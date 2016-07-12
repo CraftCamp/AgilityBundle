8b6ed6d8e4b7597cc5d81428090a4c4db2b41734
Axel Venet <kern046@gmail.com>
2016-07-12 15:04:29 +0200

    Implement tests for the new feature model and listing service

---
+++ Tests/Model/FeatureModelTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Developtech\AgilityBundle\Tests\Model;
+
+use Developtech\AgilityBundle\Tests\Mock\Feature;
+use Developtech\AgilityBundle\Tests\Mock\Project;
+use Developtech\AgilityBundle\Tests\Mock\User;
+
+class FeatureModelTest extends \PHPUnit_Framework_TestCase {
+    public function testModel() {
+        $feature =
+            (new Feature())
+            ->setId(1)
+            ->setName('Calendar')
+            ->setSlug('calendar')
+            ->setDescription('Calendar of all the events')
+            ->setDeveloper((new User()))
+            ->setProject((new Project()))
+            ->setProductOwnerValue(80)
+            ->setUserValue(67)
+            ->setStatus(Feature::STATUS_IN_PROGRESS)
+            ->setCreatedAt(new \DateTime())
+            ->setUpdatedAt(new \DateTime())
+        ;
+        $this->assertEquals(1, $feature->getId());
+        $this->assertEquals('Calendar', $feature->getName());
+        $this->assertEquals('calendar', $feature->getSlug());
+        $this->assertEquals('Calendar of all the events', $feature->getDescription());
+        $this->assertInstanceOf(User::class, $feature->getDeveloper());
+        $this->assertInstanceOf(Project::class, $feature->getProject());
+        $this->assertEquals(Feature::STATUS_IN_PROGRESS, $feature->getStatus());
+        $this->assertEquals(80, $feature->getProductOwnerValue());
+        $this->assertEquals(67, $feature->getUserValue());
+        $this->assertInstanceOf(\DateTime::class, $feature->getCreatedAt());
+        $this->assertInstanceOf(\DateTime::class, $feature->getUpdatedAt());
+    }
+}
