4d6832ea58cd4d1a44586dd1bfcba7006723850e
Axel Venet <kern046@gmail.com>
2016-07-13 22:26:53 +0200

    Implement a new creation method for features

--- Manager/FeatureManager.php
+++ Manager/FeatureManager.php
@@ -9,9 +9,13 @@ use Developtech\AgilityBundle\Model\FeatureModel;
 
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
+use Developtech\AgilityBundle\Utils\Slugger;
+
 class FeatureManager {
     /** @var EntityManager **/
     protected $em;
+    /** @var Slugger **/
+    protected $slugger;
     /** @var string **/
     protected $featureClass;
 
@@ -19,8 +23,9 @@ class FeatureManager {
      * @param EntityManager $em
      * @param string $featureClass
      */
-    public function __construct(EntityManager $em, $featureClass) {
+    public function __construct(EntityManager $em, Slugger $slugger, $featureClass) {
         $this->em = $em;
+        $this->slugger = $slugger;
         $this->featureClass = $featureClass;
     }
 
@@ -40,6 +45,33 @@ class FeatureManager {
         if(($feature = $this->em->getRepository($this->featureClass)->find($id)) === null) {
             throw new NotFoundHttpException('Feature not found');
         }
+        return $feature;
+    }
+
+    /**
+    *  @param ProjectModel $project
+     * @param string $name
+     * @param string $description
+     * @param integer $status
+     * @param integer $productOwnerValue
+     * @param UserInterface $developer
+     * @return FeatureModel
+     */
+    public function createProductOwnerFeature(ProjectModel $project, $name, $description, $status, $productOwnerValue = null, $developer = null) {
+        $feature =
+            (new $this->featureClass())
+            ->setProject($project)
+            ->setType('product-owner')
+            ->setName($name)
+            ->setSlug($this->slugger->slugify($name))
+            ->setDescription($description)
+            ->setStatus($status)
+            ->setProductOwnerValue($productOwnerValue)
+            ->setDeveloper($developer)
+        ;
+        $project->addFeature($feature);
+        $this->em->persist($feature);
+        $this->em->flush();
         return $feature;
     }
 }
