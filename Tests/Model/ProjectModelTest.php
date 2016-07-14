825cfb093436a89acd84e5e67f14d77e3180f39d
Axel Venet <kern046@gmail.com>
2016-07-14 19:39:28 +0200

    Fix project model test

--- Tests/Model/ProjectModelTest.php
+++ Tests/Model/ProjectModelTest.php
@@ -4,19 +4,20 @@ namespace Developtech\AgilityBundle\Tests\Model;
 
 use Developtech\AgilityBundle\Tests\Mock\Project;
 use Developtech\AgilityBundle\Tests\Mock\Feature;
+use Developtech\AgilityBundle\Tests\Mock\User;
 use Developtech\AgilityBundle\Tests\Mock\Feedback;
 
 class ProjectModelTest extends \PHPUnit_Framework_TestCase {
     public function testModel() {
         $feedback = new Feedback();
         $feature = new Feature();
-        $entity =
+        $project =
             (new Project())
             ->setId(1)
             ->setName('Great project')
             ->setSlug('great-project')
             ->setCreatedAt(new \DateTime())
-            ->setProductOwner((new \StdClass()))
+            ->setProductOwner((new User()))
             ->setBetaTestStatus('open')
             ->setNbBetaTesters(12)
             ->addFeature(new Feature())
@@ -26,13 +27,13 @@ class ProjectModelTest extends \PHPUnit_Framework_TestCase {
             ->addFeedback(new Feedback())
             ->removeFeedback($feedback)
         ;
-        $this->assertEquals(1, $entity->getId());
-        $this->assertEquals('Great project', $entity->getName());
-        $this->assertEquals('great-project', $entity->getSlug());
-        $this->assertInstanceOf('DateTime', $entity->getCreatedAt());
-        $this->assertInstanceOf('StdClass', $entity->getProductOwner());
-        $this->assertEquals('open', $entity->getBetaTestStatus());
-        $this->assertEquals(12, $entity->getNbBetaTesters());
+        $this->assertEquals(1, $project->getId());
+        $this->assertEquals('Great project', $project->getName());
+        $this->assertEquals('great-project', $project->getSlug());
+        $this->assertInstanceOf('DateTime', $project->getCreatedAt());
+        $this->assertInstanceOf(User::class, $project->getProductOwner());
+        $this->assertEquals('open', $project->getBetaTestStatus());
+        $this->assertEquals(12, $project->getNbBetaTesters());
         $this->assertFalse($project->hasFeature($feature));
         $this->assertFalse($project->hasFeedback($feedback));
         $this->assertCount(1, $project->getFeedbacks());
